695baf771da75425741c72c1f27c1cfdd5c6a7da
Anthony Dalke <anthony.dalke@gmail.com>
2023-12-22 11:50:02 -0600

    Began writing functions to iteratively pull session data. Continued prototyping with Jupyter notebook.

--- piastri/src/etl.py
+++ piastri/src/etl.py
@@ -1,6 +1,50 @@
 import fastf1 as ff1
+import os
+
+
+def get_years(start, end):
+    """
+    Get the list of years for which data exists.
+
+    Parameters:
+    start (int): The first year for which to retrieve data.
+    end (int): The last year for which to retrieve data.
+
+    Returns:
+    list: A list of years.
+    """
+
+    years = list(range(start, end + 1))
+
+    return years
+
+
+def get_rounds(year):
+    """
+    Get the list of round numbers for a given year.
+
+    Parameters:
+    year (int): The year for which to retrieve round numbers.
+
+    Returns:
+    list: A list of round numbers.
+    """
+
+    schedule = ff1.get_event_schedule(1994)
+    rounds = schedule.RoundNumber.to_list()
+
+    return rounds
+
 
 def main():
+    year_start = os.environ["year_start"]
+    year_end = os.environ["year_end"]
 
-if __name__ == '__main__':
+    list_years = get_years(year_start, year_end)
+
+    for year in list_years:
+        list_rounds = get_rounds(year)
+
+
+if __name__ == "__main__":
     main()
