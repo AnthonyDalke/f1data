6b0e903399601e481758927f07c7dfab14d9be6d
Anthony Dalke <anthony.dalke@gmail.com>
2025-08-04 20:24:47 -0500

    Began Python Script for Partitioning
    Started refactoring Jupyter notebook code into more robust .py script for partitioning analysis.

---
+++ streaming/src/partitioning/partitioning.py
@@ -0,0 +1,77 @@
+# Set up environment variables for Java, required by PySpark
+import os
+
+os.environ["JAVA_HOME"] = "/opt/homebrew/opt/openjdk@17"
+os.environ["PATH"] = "/opt/homebrew/opt/openjdk@17/bin:" + os.environ["PATH"]
+
+# Import necessary libraries
+import time
+from datetime import datetime
+
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+
+pd.set_option("display.max_columns", None)
+import psutil
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import count
+
+# Clear Python's module cache to install fastf1
+import sys
+
+if "typing_extensions" in sys.modules:
+    del sys.modules["typing_extensions"]
+
+import fastf1 as ff1
+
+
+def time_process(time_start: float, time_end: float, name_process: str) -> None:
+    duration = time_end - time_start
+    print(f"Duration of {name_process} process: {duration} seconds")
+
+
+# Load data for single session
+
+session_year: int = int(input("Enter the year of the session to load (e.g., 2025): "))
+session_location: str = input(
+    "Enter the location of the session to load (e.g., 'Monaco'): "
+)
+session_name: str = input(
+    "Enter the abbrevation of the session name to load (e.g., 'Q' or 'R'): "
+)
+
+load_start: float = time.time()
+
+session_object: ff1.Session = ff1.get_session(
+    session_year, session_location, session_name
+)
+session_output: ff1.SessionData = session_object.load()
+
+load_end: float = time.time()
+time_process(load_start, load_end, "session data load")
+
+# Fetch telemetry data for all drivers from session
+print(f"Fetching telemetry data for full driver list: {len(session_object.drivers)}")
+
+telemetry_data = []
+for driver in session_object.drivers:
+    try:
+        driver_data = session_object.laps.pick_drivers(driver).get_telemetry()
+        if not driver_data.empty:
+            driver_data["Driver"] = driver
+            driver_data["SessionKey"] = (
+                f"{session_object.event.EventName} {session_object.name}"
+            )
+            telemetry_data.append(driver_data)
+    except Exception as e:
+        print(f"Error while loading data for driver {driver}: {e}")
+
+# Concatenate all driver telemetry into a single dataframe
+try:
+    combined_telemetry = pd.concat(telemetry_data, ignore_index=True)
+    print(f"Records in combined telemetry data: {len(combined_telemetry)}")
+    print(f"Preview of combined telemetry data: {combined_telemetry.head()}")
+except Exception as e:
+    print(f"Error while concatenating driver telemetry data: {e}.")
