9d665c30be9490df7e1e1950e8ecda8e643b6ab6
Anthony Dalke <anthony.dalke@gmail.com>
2025-08-20 17:23:15 -0500

    Debugged Script
    Finished developing .py script. Also added output to .gitignore.

--- streaming/src/partitioning/partitioning.py
+++ streaming/src/partitioning/partitioning.py
@@ -21,88 +21,12 @@ import fastf1 as ff1
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import count
 
-from .spark_config import config_base, config_keys
+from spark_config import config_base, config_keys
 
 
 def time_process(time_start: float, time_end: float, name_process: str) -> None:
     duration = time_end - time_start
     print(f"Duration of {name_process} process: {duration} seconds")
-
-
-# Load data for single session
-session_year: int = int(input("Enter the year of the session to load (e.g., 2025): "))
-session_location: str = input(
-    "Enter the location of the session to load (e.g., 'Monaco'): "
-)
-session_name: str = input(
-    "Enter the abbrevation of the session name to load (e.g., 'Q' or 'R'): "
-)
-
-load_start: float = time.time()
-
-session_object: ff1.Session = ff1.get_session(
-    session_year, session_location, session_name
-)
-session_output: ff1.SessionData = session_object.load()
-
-load_end: float = time.time()
-time_process(load_start, load_end, "session data load")
-
-# Fetch telemetry data for all drivers from session
-print(f"Fetching telemetry data for full driver list: {len(session_object.drivers)}")
-
-telemetry_data = []
-for driver in session_object.drivers:
-    try:
-        driver_data = session_object.laps.pick_drivers(driver).get_telemetry()
-        if not driver_data.empty:
-            driver_data["Driver"] = driver
-            driver_data["SessionKey"] = (
-                f"{session_object.event.EventName} {session_object.name}"
-            )
-            telemetry_data.append(driver_data)
-    except Exception as e:
-        print(f"Error while loading data for driver {driver}: {e}")
-
-
-# Concatenate all driver telemetry into a single dataframe
-try:
-    combined_telemetry = pd.concat(telemetry_data, ignore_index=True)
-    print(f"Records in combined telemetry data: {len(combined_telemetry)}")
-    print(f"Preview of combined telemetry data: {combined_telemetry.head()}")
-except Exception as e:
-    print(f"Error while concatenating driver telemetry data: {e}.")
-
-
-# Generate histograms to assess for skew
-combined_telemetry.hist(figsize=(12, 8))
-plt.tight_layout()
-plt.show()
-
-combined_telemetry["Driver"].value_counts().plot(
-    kind="bar", figsize=(10, 4), title="Driver Distribution"
-)
-plt.ylabel("Count")
-plt.show()
-
-
-while True:
-    test_setting: str = input(
-        "Enter one of the following case-sensitive Spark Session configuration settings to test: 'master', 'repartitioning', 'driver.memory', 'shuffle.partitions', or 'serializer': "
-    )
-    if test_setting in config_base:
-        break
-    print(
-        f"Invalid Spark Session configuration setting entered. Enter one of the following case-sensitive Spark Session configuration settings to test: 'master', 'repartitioning', 'driver.memory', 'shuffle.partitions', or 'serializer'"
-    )
-test_value: str = str(
-    input(
-        f"Enter the value to test with the {test_setting} Spark Session configuration setting."
-    )
-)
-print(
-    f"Testing Spark Session configuration setting {test_setting} with a value of {test_value}."
-)
 
 
 def create_spark_session(test_setting: str, test_value: str) -> SparkSession:
@@ -119,7 +43,7 @@ def create_spark_session(test_setting: str, test_value: str) -> SparkSession:
     """
 
     # Begin with base of Spark Session configuration
-    spark_config = config_base.copy()
+    spark_config = config_base[test_setting].copy()
 
     # Assemble rest of Spark Session configuration based on test parameters
     if test_setting == "serializer":
@@ -149,3 +73,88 @@ def create_spark_session(test_setting: str, test_value: str) -> SparkSession:
             spark_builder = spark_builder.config(key, value)
 
     return spark_builder.getOrCreate()
+
+
+def main():
+    # Load data for single session
+    session_year: int = int(
+        input("Enter the year of the session to load (e.g., 2025): ")
+    )
+    session_location: str = input(
+        "Enter the location of the session to load (e.g., 'Monaco'): "
+    )
+    session_name: str = input(
+        "Enter the abbrevation of the session name to load (e.g., 'Q' or 'R'): "
+    )
+
+    load_start: float = time.time()
+
+    session_object = ff1.get_session(session_year, session_location, session_name)
+    session_object.load()
+
+    load_end: float = time.time()
+    time_process(load_start, load_end, "session data load")
+
+    # Fetch telemetry data for all drivers from session
+    print(
+        f"Fetching telemetry data for full driver list: {len(session_object.drivers)}"
+    )
+
+    telemetry_data = []
+    for driver in session_object.drivers:
+        try:
+            driver_data = session_object.laps.pick_drivers(driver).get_telemetry()
+            if not driver_data.empty:
+                driver_data["Driver"] = driver
+                driver_data["SessionKey"] = (
+                    f"{session_object.event.EventName} {session_object.name}"
+                )
+                telemetry_data.append(driver_data)
+        except Exception as e:
+            print(f"Error while loading data for driver {driver}: {e}")
+
+    # Concatenate all driver telemetry into a single dataframe
+    try:
+        combined_telemetry = pd.concat(telemetry_data, ignore_index=True)
+        print(f"Records in combined telemetry data: {len(combined_telemetry)}")
+        print(f"Preview of combined telemetry data: {combined_telemetry.head()}")
+    except Exception as e:
+        print(f"Error while concatenating driver telemetry data: {e}.")
+        combined_telemetry = pd.DataFrame()
+
+    # Generate histograms to assess for skew
+    combined_telemetry.hist(figsize=(12, 8))
+    plt.tight_layout()
+    plt.show()
+
+    combined_telemetry["Driver"].value_counts().plot(
+        kind="bar", figsize=(10, 4), title="Driver Distribution"
+    )
+    plt.ylabel("Count")
+    plt.show()
+
+    while True:
+        test_setting: str = input(
+            "Enter one of the following case-sensitive Spark Session configuration settings to test: 'master', 'repartitioning', 'driver.memory', 'shuffle.partitions', or 'serializer': "
+        )
+        if test_setting in config_base:
+            break
+        print(
+            f"Invalid Spark Session configuration setting entered. Enter one of the following case-sensitive Spark Session configuration settings to test: 'master', 'repartitioning', 'driver.memory', 'shuffle.partitions', or 'serializer': "
+        )
+    test_value: str = str(
+        input(
+            f"Enter the value to test with the {test_setting} Spark Session configuration setting."
+        )
+    )
+    print(
+        f"Testing Spark Session configuration setting {test_setting} with a value of {test_value}."
+    )
+
+    spark_session = create_spark_session(test_setting, test_value)
+    input(f"Press Enter to stop Spark session...")
+    spark_session.stop()
+
+
+if __name__ == "__main__":
+    main()
